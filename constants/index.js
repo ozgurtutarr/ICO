e5c083dca5359c5ecff30b64a8f548a5c627f811
ozgurtutarr <ozgurtutarr@gmail.com>
2022-11-03 04:10:49 +0300

    first commit

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  '0x044A30609027cADb0103Cf18c2aBf4FB679E4363';
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
